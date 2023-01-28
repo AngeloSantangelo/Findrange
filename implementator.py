2f3cd3fb920082a9480900a9f494c42e84cd8771
AngeloSantangelo <115495018+AngeloSantangelo@users.noreply.github.com>
2023-01-28 13:20:07 +0100

    Usato MLP per implementator

--- implementator.py
+++ implementator.py
@@ -1,17 +1,17 @@
 import pandas as pd
-from sklearn.ensemble import VotingClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler
 from joblib import load
 
-voting_classifier: VotingClassifier = load('price_analizer.joblib')
+mlp: MLPClassifier = load('price_analizer.joblib')
 transformer: MinMaxScaler = load("transformer.joblib")
 while True:
     battery_power = float(input("Battery power: "))
     blue = float(input("Bluetooth [Y/n]: "))
     dual_sim = float(input("Dual SIM [Y/n]: "))
     four_g = float(input("Four G [Y/n]: "))
-    int_memory = float(input("Internal memory: "))
-    ram = float(input("RAM: "))
+    int_memory = float(input("Internal memory: Giga Byte "))
+    ram = float(input("RAM: Mega Byte"))
     talk_time = float(input("Talk Time: "))
     three_g = float(input("Three G [Y/n]: "))
     touch_screen = float(input("Touch Screen [Y/n]: "))
@@ -19,7 +19,7 @@ while True:
     camera = float(input("Camera: "))
     phone = pd.DataFrame({"battery_power": [battery_power], "blue": [blue], "dual_sim": [dual_sim], "four_g": [four_g], "int_memory": [int_memory], "ram": [ram], "talk_time": [talk_time], "three_g": [three_g], "touch_screen": [touch_screen], "wifi": [wifi], "camera": [camera]})
     normalized_phone = transformer.transform(phone)
-    prediction = voting_classifier.predict(normalized_phone)
+    prediction = mlp.predict(normalized_phone)
 
     if prediction[0]==0:
         print("Your phone is in price range [200-350]€")
