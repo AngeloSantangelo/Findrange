060366a816d55432daa58d2646c187e68566d048
AngeloSantangelo <115495018+AngeloSantangelo@users.noreply.github.com>
2023-01-28 16:38:04 +0100

    Migliorato visualizzazione dell'output

--- implementator.py
+++ implementator.py
@@ -11,13 +11,17 @@ while True:
     dual_sim = float(input("Dual SIM [Y/n]: "))
     four_g = float(input("Four G [Y/n]: "))
     int_memory = float(input("Internal memory: Giga Byte "))
-    ram = float(input("RAM: Mega Byte"))
+    ram = float(input("RAM: Mega Byte "))
     talk_time = float(input("Talk Time: "))
     three_g = float(input("Three G [Y/n]: "))
     touch_screen = float(input("Touch Screen [Y/n]: "))
     wifi = float(input("WiFi [Y/n]: "))
     camera = float(input("Camera: "))
-    phone = pd.DataFrame({"battery_power": [battery_power], "blue": [blue], "dual_sim": [dual_sim], "four_g": [four_g], "int_memory": [int_memory], "ram": [ram], "talk_time": [talk_time], "three_g": [three_g], "touch_screen": [touch_screen], "wifi": [wifi], "camera": [camera]})
+    phone = pd.DataFrame({"battery_power": [battery_power], "blue": [blue], 
+    "dual_sim": [dual_sim], "four_g": [four_g], "int_memory": [int_memory], "ram": [ram], 
+    "talk_time": [talk_time], "three_g": [three_g], "touch_screen": [touch_screen], 
+    "wifi": [wifi], "camera": [camera]})
+    
     normalized_phone = transformer.transform(phone)
     prediction = mlp.predict(normalized_phone)
 
